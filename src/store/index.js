11236508aa00bfa21717fd7348a9672a7fe6cf5d
hamza-dev1 <hamza.rquiq1@gmail.com>
2022-11-08 08:53:53 +0100

    add state, getters and import module for users

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,20 @@
 import { createStore } from 'vuex'
+import usersModule from './modules/users.module'
 
 export default createStore({
   state: {
+    usersState: usersModule.state
   },
   getters: {
+    getUsersState: (state) => {
+      return state.usersState.users;
+    }
   },
   mutations: {
   },
   actions: {
   },
   modules: {
+    usersModule
   }
 })
