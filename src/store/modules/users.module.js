ab5ef2f0b1083ec165edac8a41cdcfe805e75c27
hamza-dev1 <hamza.rquiq1@gmail.com>
2022-11-11 10:07:56 +0100

    Add UPDATE_USER mutation and it corresponding action

--- src/store/modules/users.module.js
+++ src/store/modules/users.module.js
@@ -5,9 +5,19 @@ export default {
         users: UsersService.getAllUsers()
     },
     mutations: {
-
+        UPDATE_USER: (state, payload) => {
+            state.users = state.users.map((user) => {
+                if (user.id == payload.id) {
+                    console.log("user: ", user);
+                    return { ...payload }
+                }
+                return user;
+            });
+        }
     },
     actions: {
-        
+        updateUser({ commit }, payload) {
+            return commit('UPDATE_USER', payload);
+        }
     }
 }
