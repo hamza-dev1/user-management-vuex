f1247d8fb519ed039bd753c0e8be57a2393d4c90
hamza-dev1 <hamza.rquiq1@gmail.com>
2022-11-11 10:12:09 +0100

    Add condition for name if it is longer than 20 char

--- src/views/UsersView.vue
+++ src/views/UsersView.vue
@@ -11,7 +11,11 @@
     </div>
     <div class="p-5 mt-5">
         <a href="#">
-            <h5 class="mb-2 sm:text-base md:text-lg lg:text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ user.name }}</h5>
+            <h5 class="mb-2 sm:text-base md:text-lg lg:text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ 
+                user.name.length <= 20 ?
+                user.name : user.name.substring(0, 20) + "..."
+
+            }}</h5>
         </a>
         <p class="mb-3 sm:text-sm lg:text-base font-normal text-gray-700 dark:text-gray-400">{{ user.about.substring(0, 80) }}...</p>
         <router-link :to="{ name: 'userDetail', params: { id: user.id }}" class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
