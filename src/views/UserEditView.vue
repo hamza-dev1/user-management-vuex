ae2fa945d13069f089b15caf779b3e06b54dd14b
hamza-dev1 <hamza.rquiq1@gmail.com>
2022-11-12 13:40:36 +0100

    Refactoring design

--- src/views/UserEditView.vue
+++ src/views/UserEditView.vue
@@ -1,108 +1,125 @@
 <template>
-	<div class="flex">
-		<div class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 
-        w-11/12 
-        sm:w-5/6 
-        md:w-4/6 
-        lg:w-3/6 
-        mx-auto
-        ">
-			<form >
-				<div class="mb-6">
-					<label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your name</label>
-					<input
-						type="text"
-						id="name"
-						class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-						placeholder="Hamza-dev1"
-						required=""
-                        v-model="state.name"
-					/>
-				</div>
-				<div class="mb-6">
-					<label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">username</label>
-					<input
-						type="text"
-						id="name"
-						class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-						placeholder="Hamza-dev1"
-						required=""
-                        v-model="state.username"
-					/>
-				</div>
-				<div class="mb-6">
-					<label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
-					<input
-						type="email"
-						id="email"
-						class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-						placeholder="test@mail.com"
-						required=""
-                        v-model="state.email"
-					/>
-				</div>
-				<div class="mb-6">
-					<label for="number" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your phone</label>
-					<input
-						type="text"
-						id="number"
-						class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-						required=""
-                        v-model="state.phone"
-					/>
-				</div>
-				<div class="mb-6">
-                    
-<label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">About you</label>
-<textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write something about you..."
-v-model="state.about"
-></textarea>
-
-				</div>
-				<button
-                    @click.prevent="updateData"
-					type="submit"
-					class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-				>
-					Update
-				</button>
-			</form>
-		</div>
-	</div>
+	<h1 class="text-2xl mb-4">Update <span class="text-green-700 underline underline-offset-2">{{ state.name }}</span></h1>
+  <div class="flex">
+    <div
+      class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 w-11/12 sm:w-5/6 md:w-4/6 lg:w-3/6 mx-auto"
+    >
+      <form>
+        <div class="mb-6">
+          <label
+            for="name"
+            class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            >Your name</label
+          >
+          <input
+            type="text"
+            id="name"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Hamza-dev1"
+            required=""
+            v-model="state.name"
+          />
+        </div>
+        <div class="mb-6">
+          <label
+            for="username"
+            class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            >username</label
+          >
+          <input
+            type="text"
+            id="name"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Hamza-dev1"
+            required=""
+            v-model="state.username"
+          />
+        </div>
+        <div class="mb-6">
+          <label
+            for="email"
+            class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            >Your email</label
+          >
+          <input
+            type="email"
+            id="email"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="test@mail.com"
+            required=""
+            v-model="state.email"
+          />
+        </div>
+        <div class="mb-6">
+          <label
+            for="number"
+            class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            >Your phone</label
+          >
+          <input
+            type="text"
+            id="number"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            required=""
+            v-model="state.phone"
+          />
+        </div>
+        <div class="mb-6">
+          <label
+            for="message"
+            class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+            >About you</label
+          >
+          <textarea
+            id="message"
+            rows="4"
+            class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Write something about you..."
+            v-model="state.about"
+          ></textarea>
+        </div>
+        <button
+          @click.prevent="updateData"
+          type="submit"
+          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Update
+        </button>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
-import { computed, reactive } from 'vue'
-import { useStore } from 'vuex'
-import { useRoute } from 'vue-router'
+import { computed, reactive } from "vue";
+import { useStore } from "vuex";
+import { useRoute } from "vue-router";
 export default {
-    name: 'UserEditView',
-    setup() {
-        const store = useStore();
-        const route  = useRoute();
-        const userId = route.params.id;
-        const userState = computed(() => store.getters.getUser(userId));
-        const state = reactive({
-            id: userState.value.id,
-            name: userState.value.name,
-            username: userState.value.username,
-            email: userState.value.email,
-            phone: userState.value.phone,
-            about: userState.value.about,
-            image: userState.value.image,
-        });
-        const updateData = () => {
-            store.dispatch('usersModule/updateUser', state);
-        }
-        
-        return {
-            state,
-            updateData
-        }
-    }
-}
+  name: "UserEditView",
+  setup() {
+    const store = useStore();
+    const route = useRoute();
+    const userId = route.params.id;
+    const userState = computed(() => store.getters.getUser(userId));
+    const state = reactive({
+      id: userState.value.id,
+      name: userState.value.name,
+      username: userState.value.username,
+      email: userState.value.email,
+      phone: userState.value.phone,
+      about: userState.value.about,
+      image: userState.value.image,
+    });
+    const updateData = () => {
+      store.dispatch("usersModule/updateUser", state);
+    };
+
+    return {
+      state,
+      updateData,
+    };
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/views/UsersView.vue
+++ src/views/UsersView.vue
@@ -1,56 +1,85 @@
 <template>
-    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 gap-4 justify-items-center users mx-auto">
-<div class="relative max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700" v-for="user in users" :key="user">
-    <div class="relative card-header">
+    <h1 class="text-2xl mb-4">All users</h1>
+  <div
+    class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 gap-4 justify-items-center mx-auto"
+  >
+    <div
+      class="relative max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700"
+      v-for="user in users"
+      :key="user"
+    >
+      <div class="relative card-header">
         <div class="cover">
-            <img class="rounded-t-lg" src="../assets/images/cover.jpg" alt="">
+          <img class="rounded-t-lg" src="../assets/images/cover.jpg" alt="" />
         </div>
         <div class="absolute -bottom-10 w-full profile-pic">
-            <img class="mx-auto mb-3 w-24 h-24 rounded-full shadow-lg border-4 border-white" :src="user.image" alt="Bonnie image"/>
+          <img
+            class="mx-auto mb-3 w-24 h-24 rounded-full shadow-lg border-4 border-white"
+            :src="user.image"
+            alt="Bonnie image"
+          />
         </div>
-    </div>
-    <div class="p-5 mb-10 mt-5">
+      </div>
+      <div class="p-5 mb-10 mt-5">
         <a href="#">
-            <h5 class="mb-2 sm:text-base md:text-lg lg:text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ 
-                user.name.length <= 20 ?
-                user.name : 
-                (user.name.substring(0, 20) + "...")
-
-            }}</h5>
+          <h5
+            class="mb-2 sm:text-base md:text-lg lg:text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+          >
+            {{
+              user.name.length <= 20
+                ? user.name
+                : user.name.substring(0, 20) + "..."
+            }}
+          </h5>
         </a>
-        <p class="mb-3 sm:text-sm lg:text-base font-normal text-gray-700 dark:text-gray-400">{{ 
-            user.about.length <= 80 ?
-            user.about :
-            (user.about.substring(0, 80) + "...")
-        }}</p>
-        <router-link :to="{ name: 'userDetail', params: { id: user.id }}" class="absolute bottom-3 inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            Details
-            <svg aria-hidden="true" class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+        <p
+          class="mb-3 sm:text-sm lg:text-base font-normal text-gray-700 dark:text-gray-400"
+        >
+          {{
+            user.about.length <= 80
+              ? user.about
+              : user.about.substring(0, 80) + "..."
+          }}
+        </p>
+        <router-link
+          :to="{ name: 'userDetail', params: { id: user.id } }"
+          class="absolute bottom-3 inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Details
+          <svg
+            aria-hidden="true"
+            class="ml-2 -mr-1 w-4 h-4"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
         </router-link>
+      </div>
     </div>
-</div>
-    </div>
+  </div>
 </template>
 
 <script>
-import { computed, ref } from 'vue'
-import { useStore } from 'vuex'
+import { computed, ref } from "vue";
+import { useStore } from "vuex";
 
 export default {
-    name: 'UsersView',
-    setup() {
-        const store = useStore();
-        const usersState = computed(() => store.getters.getUsersState)
-        const users = ref([
-            ...usersState.value.users
-        ]);
-        return {
-            users
-        }
-    }
-}
+  name: "UsersView",
+  setup() {
+    const store = useStore();
+    const usersState = computed(() => store.getters.getUsersState);
+    const users = ref([...usersState.value.users]);
+    return {
+      users,
+    };
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
